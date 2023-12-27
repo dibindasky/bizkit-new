00ae1d4749fa21f432b5ef2060bff04d6d09c2af
dibindasky2000@gmail.com <dibindasky2000@gmail.com>
2023-12-27 14:54:59 +0530

    make a bizkit card page done

--- lib/feature/selfie_card/view/selfie_screens_list.dart
+++ lib/feature/selfie_card/view/selfie_screens_list.dart
@@ -1,23 +1,22 @@
-import 'package:bizkit/feature/selfie_card/widgets/selfie_preview_screen.dart';
 import 'package:bizkit/feature/selfie_card/widgets/selfie_screen.dart';
 import 'package:flutter/material.dart';
 
 class SelfieScreensList extends StatefulWidget {
-  SelfieScreensList({super.key});
+  const SelfieScreensList({super.key});
 
   @override
   State<SelfieScreensList> createState() => _SelfieScreensListState();
 }
 
+ List<Widget> screensSelfi = [
+  SelfieScreen(),
+];
+
 class _SelfieScreensListState extends State<SelfieScreensList> {
-  final screens = [
-    SelfieScreen(),
-    SelfiePreviewScreen(),
-  ];
   int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return screens[index];
+    return screensSelfi.last;
   }
 }

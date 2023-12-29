c8c16b67d0f112ea63bbd1e8162280660dc5f902
dibindasky2000@gmail.com <dibindasky2000@gmail.com>
2023-12-29 13:10:09 +0530

    reminder screen

--- lib/feature/home/view/widgets/reminder_screen.dart
+++ lib/feature/home/view/widgets/reminder_screen.dart
@@ -9,7 +9,6 @@ class AddReminderScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: knill,
@@ -20,61 +19,69 @@ class AddReminderScreen extends StatelessWidget {
           },
           icon: const Icon(
             Icons.arrow_back_ios,
-
           ),
-          title: const Text('Alex Adams'),
-          actions: [
-            IconButton(
-                onPressed: () {}, icon: const Icon(Icons.more_vert_outlined))
-          ],
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TTextFormField(
+        title: const Text('Alex Adams'),
+        actions: [
+          IconButton(
+              onPressed: () {}, icon: const Icon(Icons.more_vert_outlined))
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TTextFormField(
                   suffix: Icon(
                     Icons.edit_outlined,
                     color: kwhite.withOpacity(0.5),
                   ),
                   text: 'Write Meeting Label',
                   controller: TextEditingController(),
-                  inputType: TextInputType.text,
+                  inputType: TextInputType.text),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: kwidth * 0.3,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: textFieldFillColr,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
+                  ),
                 ),
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  height: kwidth * 0.3,
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: textFieldFillColr,
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(7),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Message',
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: klightgrey,
+                          fontSize: kwidth * 0.04),
                     ),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Message',
-                        style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: klightgrey,
-                            fontSize: kwidth * 0.04),
+                    TextField(
+                      controller: TextEditingController(),
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Write here',
+                        hintStyle: TextStyle(
+                            color: klightgrey, fontSize: kwidth * 0.03),
                       ),
-                      TextField(
-                        controller: TextEditingController(),
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: 'Write here',
-                          hintStyle: TextStyle(
-                              color: klightgrey, fontSize: kwidth * 0.03),
-                        ),
-                      ),
-                    ],
+                    ),
+                  ],
+                ),
+              ),
+              adjustHieght(kwidth * 0.03),
+              Container(
+                padding: const EdgeInsets.all(10),
+                height: kwidth * 0.13,
+                decoration: const BoxDecoration(
+                  color: textFieldFillColr,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(7),
                   ),
                 ),
-
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
@@ -93,29 +100,13 @@ class AddReminderScreen extends StatelessWidget {
                 child: ColoredBox(
                   color: kwhite.withOpacity(0.05),
                   child: Column(
-
                     children: [
-                      Expanded(
-                          child: TextField(
-                        controller: dateController,
-                      )),
-                    ],
-                  ),
-                ),
-                ClipRRect(
-                  borderRadius: const BorderRadius.all(Radius.circular(8)),
-                  child: ColoredBox(
-                    color: kwhite.withOpacity(0.05),
-                    child: Column(
-                      children: [
-                        CalendarDatePicker(
+                      CalendarDatePicker(
                           initialDate: DateTime.now(),
                           firstDate: DateTime.now(),
                           lastDate: DateTime.now().add(
                             const Duration(days: 365 * 100),
                           ),
-
-
                           onDateChanged: (date) {}),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.end,
@@ -131,42 +122,34 @@ class AddReminderScreen extends StatelessWidget {
                         ],
                       )
                     ],
-
                   ),
                 ),
-                adjustHieght(kwidth * 0.10),
-                Row(
-                  children: [
-                    adjustWidth(kwidth * 0.30),
-                    Expanded(
-                        child: GestureDetector(
-                      onTap: () => Navigator.of(context).pop(),
+              ),
+              adjustHieght(kwidth * 0.10),
+              Row(
+                children: [
+                  adjustWidth(kwidth * 0.30),
+                  Expanded(
                       child: Container(
-                        height: kwidth * 0.1,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: neonShade),
-                            borderRadius:
-                                const BorderRadius.all(Radius.circular(8))),
-                        child: const Center(
-                          child: Text('Cancel'),
-                        ),
-                      ),
-                    )),
-                    adjustWidth(kwidth * 0.10),
-                    Expanded(
-                        child: Container(
-                      height: kwidth * 0.1,
-                      decoration: const BoxDecoration(
-                          color: textFieldFillColr,
-                          borderRadius: BorderRadius.all(Radius.circular(8))),
-                      child: const Center(
-                        child: Text('Save & Notify'),
-                      ),
-                    ))
-                  ],
-                )
-              ],
-            ),
+                    height: kwidth * 0.1,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: neonShade),
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(8))),
+                    child: const Center(child: Text('Cancel')),
+                  )),
+                  adjustWidth(kwidth * 0.10),
+                  Expanded(
+                      child: Container(
+                    height: kwidth * 0.1,
+                    decoration: const BoxDecoration(
+                        color: textFieldFillColr,
+                        borderRadius: BorderRadius.all(Radius.circular(8))),
+                    child: const Center(child: Text('Save & Notify')),
+                  ))
+                ],
+              )
+            ],
           ),
         ),
       ),

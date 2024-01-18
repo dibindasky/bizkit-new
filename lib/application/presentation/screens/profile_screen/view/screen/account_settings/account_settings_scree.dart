3c3bba02ced3cdb6b076a9124f4290a0439466ca
Febin Baby <febinbaby879@gmail.com>
2024-01-18 15:22:46 +0530

    presnetation folder changed

---
+++ lib/application/presentation/screens/profile_screen/view/screen/account_settings/account_settings_scree.dart
@@ -0,0 +1,39 @@
+import 'package:bizkit/application/presentation/screens/profile_screen/view/widgets/tile_item.dart';
+import 'package:flutter/material.dart';
+import 'package:bizkit/application/commen/const.dart';
+
+class AccountSettigsScreen extends StatelessWidget {
+  const AccountSettigsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () => Navigator.of(context).pop(),
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 20,
+            color: kwhite,
+          ),
+        ),
+        backgroundColor: knill,
+        title: const Text(
+          'Account Settings',
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            TileItem(text: 'Change Username'),
+            TileItem(text: 'Change Password'),
+            TileItem(text: 'Verify Email'),
+            TileItem(text: 'Logged In Devices'),
+            TileItem(text: 'Notification Settings'),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/application/presentation/screens/profile_screen/view/screen/privacy_security/privacy_screen.dart
@@ -0,0 +1,39 @@
+import 'package:bizkit/application/commen/const.dart';
+import 'package:bizkit/application/presentation/screens/profile_screen/view/widgets/tile_item.dart';
+import 'package:flutter/material.dart';
+
+class PrivacyAndSecurityScreen extends StatelessWidget {
+  const PrivacyAndSecurityScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios,
+            size: 18,
+            color: kwhite,
+          ),
+        ),
+        backgroundColor: knill,
+        title: const Text(
+          'Privacy and Security',
+          style: TextStyle(color: kwhite),
+        ),
+      ),
+      body: const Padding(
+        padding: EdgeInsets.symmetric(horizontal: 15),
+        child: Column(
+          children: [
+            TileItem(text: 'Level Setting'),
+            TileItem(text: 'Privacy Settings'),
+          ],
+        ),
+      ),
+    );
+  }
+}

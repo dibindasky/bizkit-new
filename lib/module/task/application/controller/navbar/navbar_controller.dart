b8d19fdb48ef16e60ae9517abc1570caada65878
dibindasky2000@gmail.com <dibindasky2000@gmail.com>
2024-05-23 18:16:46 +0530

    kjfhskj

--- lib/module/task/application/controller/navbar/navbar_controller.dart
+++ lib/module/task/application/controller/navbar/navbar_controller.dart
@@ -1,7 +1,9 @@
 import 'package:get/get.dart';
 
 class TaskNavbarController extends GetxController {
+  
   RxInt taskBottomIndex = 0.obs;
+
   changeBottomIndex(int index) {
     taskBottomIndex.value = index;
   }

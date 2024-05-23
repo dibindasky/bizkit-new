a49ea1b7e2168e91b02562a351ee1b19e4662ed7
Febin baby <febinbaby879@gmail.com>
2024-05-23 18:12:43 +0530

    aded

---
+++ lib/module/task/application/presentation/screens/total_tasks/widgets/custom_pop_menubutton.dart
@@ -0,0 +1,39 @@
+import 'package:bizkit/utils/constants/colors.dart';
+import 'package:bizkit/utils/constants/contants.dart';
+import 'package:flutter/material.dart';
+
+class CustomPopupMenuItem extends StatelessWidget {
+  final String text;
+  final VoidCallback onTap;
+
+  const CustomPopupMenuItem({
+    super.key,
+    required this.text,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.pop(context);
+        onTap;
+      },
+      child: Container(
+        padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [neonShade, neonShade, lightGrey],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          borderRadius: kBorderRadius10,
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(color: kwhite, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}

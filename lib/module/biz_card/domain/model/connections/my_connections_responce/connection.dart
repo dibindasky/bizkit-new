635fca062bf9a090ccb2b0f9a424886f2e56a178
Jibil PT <jibilptjibil@gmail.com>
2024-10-18 18:34:35 +0530

    completed myconnection local db

--- lib/module/biz_card/domain/model/connections/my_connections_responce/connection.dart
+++ lib/module/biz_card/domain/model/connections/my_connections_responce/connection.dart
@@ -1,10 +1,9 @@
+import 'dart:developer';
+
 import 'package:json_annotation/json_annotation.dart';
-
 import 'card.dart';
-
 part 'connection.g.dart';
 
-@JsonSerializable()
 class MyConnection {
   @JsonKey(name: 'to_user')
   int? localId;
@@ -20,7 +19,6 @@ class MyConnection {
 
   Map<String, dynamic> toJson() => _$ConnectionToJson(this);
 
-  // equals function to compare MyConnection objects
   bool equals(MyConnection other) {
     return localId == other.localId &&
            toUser == other.toUser &&
@@ -28,28 +26,43 @@ class MyConnection {
            _compareCardLists(cards, other.cards);
   }
 
-  // Updated function to compare lists of Card objects (ignores order)
   bool _compareCardLists(List<Card>? list1, List<Card>? list2) {
+    log('compare card List 1');
     if (list1 == null && list2 == null) return true;
+    log('compare card List 2');
     if (list1 == null || list2 == null || list1.length != list2.length) return false;
+    log('compare card List 3');
 
-    // Create maps for easier comparison by connectionId
     final map1 = {for (var card in list1) card.toCard: card};
     final map2 = {for (var card in list2) card.toCard: card};
 
     if (map1.length != map2.length) return false;
 
     for (var id in map1.keys) {
-      if (!map2.containsKey(id)) return false; // Different connectionId
-      if (!map1[id]!.equals(map2[id]!)) return false; // Compare card fields
+      if (!map2.containsKey(id)) return false;
+      if (!map1[id]!.equals(map2[id]!)) return false;
     }
 
     return true;
   }
 
-  // Static constants
   static const String colCurrentUserId = 'current_user_id';
   static const String colLocalId = 'local_id';
   static const String colToUser = 'to_user';
   static const String colUserNmae = 'username';
+
+  // CopyWith method
+  MyConnection copyWith({
+    int? localId,
+    String? toUser,
+    String? username,
+    List<Card>? cards,
+  }) {
+    return MyConnection(
+      localId: localId ?? this.localId,
+      toUser: toUser ?? this.toUser,
+      username: username ?? this.username,
+      cards: cards ?? this.cards,
+    );
+  }
 }

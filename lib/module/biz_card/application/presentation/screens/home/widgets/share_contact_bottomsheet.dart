fb35430288ccbcd5997bdf47670f17441e8ac8ec
dibindasky2000@gmail.com <dibindasky2000@gmail.com>
2024-11-23 15:23:52 +0530

    done dome

--- lib/module/biz_card/application/presentation/screens/home/widgets/share_contact_bottomsheet.dart
+++ lib/module/biz_card/application/presentation/screens/home/widgets/share_contact_bottomsheet.dart
@@ -9,6 +9,7 @@ import 'package:bizkit/utils/shimmer/shimmer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 
 class ShareCardThroughContactBottomSheet extends StatelessWidget {
   const ShareCardThroughContactBottomSheet({super.key, required this.cardId});
@@ -37,55 +38,62 @@ class ShareCardThroughContactBottomSheet extends StatelessWidget {
                   ),
                 ),
                 Expanded(
-                  child: controller.fetchingLoading.value
-                      ? ShimmerLoader(
-                          itemCount: 40,
-                          height: 50.h,
-                          width: double.infinity,
-                          seprator: kHeight5)
-                      : controller.contactList.isEmpty
-                          ? const Center(child: Text('No Contacts Found'))
-                          : ListView.builder(
-                              itemCount: controller.contactList.length,
-                              itemBuilder: (context, index) {
-                                final data = controller.contactList[index];
-                                final selected = (controller
-                                            .shareCardContactModel
-                                            .value
-                                            .contacts ??
-                                        <ShareCardContact>[])
-                                    .any((element) =>
-                                        element.email == data.email &&
-                                        element.phoneNumber ==
-                                            data.phoneNumber);
-                                return ListTile(
-                                    onTap: () =>
-                                        controller.addOrRemoveContactToList(
-                                            model: data, selected: selected),
-                                    title: Text(data.name ?? ''),
-                                    leading: data.profilePicture != null &&
-                                            data.profilePicture != ''
-                                        ? CircleAvatar(
-                                            backgroundColor: kgrey,
-                                            backgroundImage: MemoryImage(
-                                                base64Decode(
-                                                    data.profilePicture ?? '')))
-                                        : const CircleAvatar(
-                                            backgroundColor: kgrey,
-                                            backgroundImage:
-                                                AssetImage(imagePerson)),
-                                    trailing: Wrap(
-                                      children: [
-                                        selected
-                                            ? const Icon(
-                                                Icons.check_box_outlined,
-                                                color: kneonShade)
-                                            : const Icon(
-                                                Icons.check_box_outline_blank),
-                                      ],
-                                    ));
-                              },
-                            ),
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: controller.fetchingLoading.value
+                        ? ShimmerLoader(
+                            itemCount: 40,
+                            height: 50.h,
+                            width: double.infinity,
+                            seprator: kHeight5)
+                        : controller.contactList.isEmpty
+                            ? const Center(child: Text('No Contacts Found'))
+                            : ListView.builder(
+                                itemCount: controller.contactList.length,
+                                itemBuilder: (context, index) {
+                                  final data = controller.contactList[index];
+                                  final selected = (controller
+                                              .shareCardContactModel
+                                              .value
+                                              .contacts ??
+                                          <ShareCardContact>[])
+                                      .any((element) =>
+                                          element.email == data.email &&
+                                          element.phoneNumber ==
+                                              data.phoneNumber);
+                                  return ListTile(
+                                      onTap: () =>
+                                          controller.addOrRemoveContactToList(
+                                              model: data, selected: selected),
+                                      title: Text(data.name ?? ''),
+                                      leading: 
+                                      // data.profilePicture != null &&
+                                      //         data.profilePicture != ''
+                                      //     ? CircleAvatar(
+                                      //         backgroundColor: kblack,
+                                      //         backgroundImage: MemoryImage(
+                                      //             base64Decode(
+                                      //                 data.profilePicture ??
+                                      //                     '')))
+                                      //     :
+                                           const CircleAvatar(
+                                              backgroundColor: kblack,
+                                              child: Icon(Iconsax.profile_2user,
+                                                  color: kGreyNormal),
+                                            ),
+                                      trailing: Wrap(
+                                        children: [
+                                          selected
+                                              ? const Icon(
+                                                  Icons.check_box_outlined,
+                                                  color: kneonShade)
+                                              : const Icon(Icons
+                                                  .check_box_outline_blank),
+                                        ],
+                                      ));
+                                },
+                              ),
+                  ),
                 ),
                 Obx(() {
                   return controller.cardSharingLoading.value
